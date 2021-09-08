07b46d0b4d22ef4ee7b8e0a1d040d931e9f29b08
Tina Zhu <tinazhu@Tinas-MacBook-Pro.local>
2021-09-08 01:03:38 -0700

    amazon linux 2 is centos, full build LAME from source

--- .platform/hooks/prebuild/setup.sh
+++ .platform/hooks/prebuild/setup.sh
@@ -1,3 +1,15 @@
-echo "Installing LAME"
-sudo yum install lame
-echo "Finished LAME install"
+echo "installing tools needed to build LAME"
+yum update
+yum install gcc gcc-c++ automake autoconf libtool yasm nasm git subversion
+echo "getting the tarball"
+cd /usr/local/src
+wget https://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz/download
+tar -xvf lame-3.99.5.tar.gz
+rm lame-3.99.5.tar.gz
+cd lame-3.99.5
+echo "installing LAME"
+./configure
+make
+make install
+echo "finished LAME install"
+# service nginx reload
