dcb864a591a63ec70df25fcc9f339510967dda46
Tina Zhu <tinazhu@Tinas-MacBook-Pro.local>
2021-09-08 01:40:03 -0700

    fix lame download problem

--- .platform/hooks/prebuild/setup.sh
+++ .platform/hooks/prebuild/setup.sh
@@ -4,7 +4,7 @@ yum update
 yum install gcc gcc-c++ automake autoconf libtool yasm nasm git subversion
 echo "getting the tarball"
 cd /usr/local/src
-wget https://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz/download
+wget -O lame-3.99.5.tar.gz https://sourceforge.net/projects/lame/files/lame/3.99/lame-3.99.5.tar.gz/download
 tar -xvf lame-3.99.5.tar.gz
 rm lame-3.99.5.tar.gz
 cd lame-3.99.5
